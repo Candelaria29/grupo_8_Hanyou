a8bdd162a2accdd05c88b5339e32d0f339997b47
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-08-05 19:29:01 -0300

    instalación de motor ejs y traslado de todos los html a ejs

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 
-const rutaPublic = path.resolve(__dirname, './public')
+const rutaPublic = path.resolve(__dirname, "./public");
 
-app.listen (8000, () => {
-    console.log ('Servidor funcionando en http://localhost:8000')
-})
+app.listen(8000, () => {
+  console.log("Servidor funcionando en http://localhost:8000");
+});
 
-app.use(express.static(rutaPublic))
+app.use(express.static(rutaPublic));
 
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/index.html')))
-app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
-app.get('/productCart', (req,res) => res.sendFile(path.resolve(__dirname, './views/productCart.html')))
-app.get('/productDetail', (req,res) => res.sendFile(path.resolve(__dirname, './views/productDetail.html')))
-app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/index.ejs"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.ejs"))
+);
+app.get("/productCart", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/productCart.ejs"))
+);
+app.get("/productDetail", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.ejs"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.ejs"))
+);
