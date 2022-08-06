9a7ff0d103dec58be82fa57cc6db23e457ec3af0
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-08-05 21:32:14 -0300

    agregue carpeta src, agregue favicon al head y cree el ejs de crear nuevo producto

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const rutaPublic = path.resolve(__dirname, "./public");
+const rutaPublic = path.resolve(__dirname, "../public");
 
 app.listen(8000, () => {
   console.log("Servidor funcionando en http://localhost:8000");
@@ -12,6 +12,8 @@ app.use(express.static(rutaPublic));
 
 app.set("view engine", "ejs");
 
+app.set("views", __dirname + "/views");
+
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -19,3 +21,5 @@ app.get("/login", (req, res) => res.render("login"));
 app.get("/productCart", (req, res) => res.render("productCart"));
 app.get("/productDetail", (req, res) => res.render("productDetail"));
 app.get("/register", (req, res) => res.render("register"));
+app.get("/createNewProduct", (req, res) => res.render("createNewProduct"));
+app.get("/editProduct", (req, res) => res.render("editProduct"));
