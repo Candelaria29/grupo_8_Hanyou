c8fe29730c9add3b458b1d3774b19f74a955e674
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-08-22 15:49:22 -0300

    Agregue pseudocodigo comentando en el productRoutes

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -10,7 +10,10 @@ const {
 // READ:
 // List:
 route.get("/", index);
-// Seria un read de algunos, ni todos ni uno sólo, si no los enviados al carrito, pero hay que armarlo:
+//El de abajo, seria un read de algunos, ni todos ni uno sólo, si no los enviados al carrito,
+//pero hay que armarlo. Seria muy similar al one del model, pero en lugar de usar
+//un find, se podria usar un filter. Por otro lado, podriamos agregarle al json un
+//campo con valor boolean para identificar a los productos agregados al carrito:
 route.get("/productos/productCart", (req, res) =>
   res.render("products/productCart")
 );
