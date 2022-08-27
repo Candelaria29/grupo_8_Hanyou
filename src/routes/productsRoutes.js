8f1c49e9931cd63019446e6f2b4a7a455f04a8ee
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-08-27 19:10:39 -0300

    Configure el middleware de multer en el productRoutes.js

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -8,16 +8,38 @@ const {
   edit,
   update,
 } = require("../controllers/productsController");
+const { resolve, extname } = require("path");
+const { existsSync, mkdirSync } = require("fs");
+const path = require("path");
 
-const destination = function (req, file, cb) {};
+const destination = function (req, file, cb) {
+  let folder = resolve(__dirname, "..", "..", "public", "products");
+  if (!existsSync(folder)) {
+    mkdirSync(folder);
+  }
+  return cb(null, folder);
+};
+
+const filename = function (req, file, cb) {
+  let unique = Date.now() + "-" + Math.round(Math.random() * 1e9);
+  let name = file.fieldname + "-" + unique + extname(file.originalname);
+  return cb(null, name);
+};
+
+const multer = require("multer");
+const { diskStorage } = require("multer");
+
+const upload = multer({
+  storage: multer.diskStorage({ destination, filename }),
+});
 
 // READ:
 // List:
 route.get("/", index);
 //El de abajo, seria un read de algunos, ni todos ni uno sólo, si no los enviados al carrito.
-//Seria muy similar al one del model, pero en lugar de usar
-//un find, se podria usar un filter. Por otro lado, podriamos agregarle al json un
-//campo con valor boolean para identificar a los productos agregados al carrito:
+//Seria muy similar al one del model, pero en lugar de usar un find, se podria usar un filter.
+// Por otro lado, podriamos agregarle al json un campo con valor boolean
+// para identificar a los productos agregados al carrito:
 route.get("/productos/productCart", (req, res) =>
   res.render("products/productCart")
 );
@@ -28,13 +50,13 @@ route.get("/productos/detalle/:sku", show);
 //Envio:
 route.get("/productos/crear", create);
 //Creacion:
-route.post("/productos/guardar", save);
+route.post("/productos/guardar", upload.any(), save);
 
 //UPDATE:
 //Envio:
 route.get("/productos/editar/:sku", edit);
 //Edicion:
-route.put("/productos/actualizar", update);
+route.put("/productos/actualizar", upload.any(), update);
 //DELETE:
 //Borrado:
 
