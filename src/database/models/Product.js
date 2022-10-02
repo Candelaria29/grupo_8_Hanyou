191e791687aae7f9a543ca4be1cb6ebf922578f1
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-10-02 15:33:35 -0300

    Complete el modelo de la entidad de productos, sin las relaciones

---
+++ src/database/models/Product.js
@@ -0,0 +1,44 @@
+module.exports = (sequelize, dataTypes) => {
+  let alias = "Product";
+  let cols = {
+    name: {
+      type: dataTypes.STRING(100),
+      allowNull: false,
+    },
+    description: {
+      type: dataTypes.STRING(280),
+      allowNull: false,
+    },
+    color: {
+      type: dataTypes.STRING(200),
+      allowNull: false,
+    },
+    size_id: {
+      type: dataTypes.INTEGER(10).UNSIGNED,
+      allowNull: false,
+    },
+    price: {
+      type: dataTypes.DECIMAL(6, 2).UNSIGNED,
+      allowNull: false,
+    },
+    sku: {
+      type: dataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    image: {
+      type: dataTypes.STRING(300),
+      allowNull: false,
+    },
+    index: {
+      type: dataTypes.TINYINT.UNSIGNED,
+      allowNull: false,
+    },
+  };
+  let config = {
+    tableName: "Products",
+    timestamps: false,
+  };
+  const Product = sequelize.define(alias, cols, config);
+  return Product;
+};
