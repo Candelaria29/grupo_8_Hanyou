2ae00bb4a289f9f5e817f0837edea119c9e99438
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-10-07 19:05:16 -0300

    Migre el metodo de logueo del controlador de usuarios hacia la base de datos en mysql

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -21,7 +21,7 @@ module.exports = {
       req.body.imagenUsuario = "default.png";
     }
     let mailOwners = req.body.email.split("@");
-    let adminMails = ["nicolasagustincilio", "candelariabarrios"];
+    let adminMails = ["nicolasagustincilio", "candelariabarrios", "nicolo"];
     Users.create({
       firstName: req.body.nombre,
       lastName: req.body.apellido,
@@ -32,6 +32,8 @@ module.exports = {
     }).then(() => {
       return res.redirect("/login");
     });
+
+    // JSON controller:
     // if (req.files && req.files.length > 0) {
     //   req.body.imagenUsuario = req.files[0].filename;
     // } else {
@@ -44,27 +46,48 @@ module.exports = {
     // return res.redirect("/login");
   },
   access: (req, res) => {
-    let allUsers = index();
-    let listOfEmails = allUsers.map((e) => e.email);
-    let result = allUsers.find((e) => e.email == req.body.userName);
-    if (listOfEmails.indexOf(req.body.userName) == -1) {
-      errorMessage = "Usuario no encontrado";
-      return res.redirect("/login");
-    } else if (!compareSync(req.body.password, result.password)) {
-      newErrorMessage = "Contraseña incorrecta";
-      return res.redirect("/login");
-    }
-    //La cookieDuration esta en milesimas (ahora dura una hora):
-    cookieDuration = 3.6e6;
-    if (req.body.remember) {
-      res.cookie("user", req.body.userName, { maxAge: cookieDuration });
-    }
-    req.session.user = allUsers.find((e) => e.email == req.body.userName);
-    return res.redirect("/profile");
+    Users.findOne({
+      where: {
+        email: req.body.userName,
+      },
+    })
+      .then((responseEmail) => {
+        if (!compareSync(req.body.password, responseEmail.password)) {
+          return res.redirect("/login");
+        }
+        //La cookieDuration esta en milesimas (ahora dura una hora):
+        cookieDuration = 3.6e6;
+        if (req.body.remember) {
+          res.cookie("user", req.body.userName, { maxAge: cookieDuration });
+        }
+        req.session.user = responseEmail;
+        return res.redirect("/profile");
+      })
+      .catch(() => res.redirect("/login"));
+
+    // JSON controller:
+    // let allUsers = index();
+    // let listOfEmails = allUsers.map((e) => e.email);
+    // let result = allUsers.find((e) => e.email == req.body.userName);
+    // if (listOfEmails.indexOf(req.body.userName) == -1) {
+    //   errorMessage = "Usuario no encontrado";
+    //   return res.redirect("/login");
+    // } else if (!compareSync(req.body.password, result.password)) {
+    //   newErrorMessage = "Contraseña incorrecta";
+    //   return res.redirect("/login");
+    // }
+    // //La cookieDuration esta en milesimas (ahora dura una hora):
+    // cookieDuration = 3.6e6;
+    // if (req.body.remember) {
+    //   res.cookie("user", req.body.userName, { maxAge: cookieDuration });
+    // }
+    // req.session.user = allUsers.find((e) => e.email == req.body.userName);
+    // return res.redirect("/profile");
   },
   logout: (req, res) => {
     delete req.session.user;
     res.cookie("user", null, { maxAge: -1 });
     return res.redirect("/");
   },
+  destroy: (req, res) => {},
 };
