22f2d1ad4fe79c0f9530402a292774db4cf04c30
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-10-07 14:03:12 -0300

    Adapte el metodo save del controlador de usuarios a Sequelize

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,5 +1,11 @@
 const { index, one, generate, write } = require("../models/usersModel");
 const { compareSync } = require("bcrypt");
+const db = require("../database/models");
+const sequelize = db.sequelize;
+const { Op } = require("sequelize");
+const { hashSync } = require("bcrypt");
+
+const Users = db.User;
 
 module.exports = {
   login: (req, res) => res.render("users/login"),
@@ -14,11 +20,28 @@ module.exports = {
     } else {
       req.body.imagenUsuario = "default.png";
     }
-    let nuevoUsuario = generate(req.body);
-    let todos = index();
-    todos.push(nuevoUsuario);
-    write(todos);
-    return res.redirect("/login");
+    let mailOwners = req.body.email.split("@");
+    let adminMails = ["nicolasagustincilio", "candelariabarrios"];
+    Users.create({
+      firstName: req.body.nombre,
+      lastName: req.body.apellido,
+      email: req.body.email,
+      password: hashSync(req.body.password, 10),
+      avatar: req.body.imagenUsuario,
+      adminType: adminMails.indexOf(mailOwners[0]) != -1 ? 1 : 0,
+    }).then(() => {
+      return res.redirect("/login");
+    });
+    // if (req.files && req.files.length > 0) {
+    //   req.body.imagenUsuario = req.files[0].filename;
+    // } else {
+    //   req.body.imagenUsuario = "default.png";
+    // }
+    // let nuevoUsuario = generate(req.body);
+    // let todos = index();
+    // todos.push(nuevoUsuario);
+    // write(todos);
+    // return res.redirect("/login");
   },
   access: (req, res) => {
     let allUsers = index();
