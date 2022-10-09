f97553c36f206328def8c40b2ec95fcaf5ae93b2
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-10-09 14:36:52 -0300

    Adapte el método de index a mysql

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,24 +2,34 @@ const { all, one, generate, write } = require("../models/productsModel");
 const { unlinkSync } = require("fs");
 const { resolve } = require("path");
 
-const db = require('../database/models/index');
+const db = require("../database/models/index");
 const { request } = require("http");
 
 const controller = {
-  //esta funcion filtra los productos destacados para mostrarlos en el index - PENDIENTE
+  //esta funcion filtra los productos destacados para mostrarlos en el index
   home: (req, res) => {
-    let products = all();
-    let indexProducts = products.filter((product) => product.index == "1");
-    res.render("index", { indexProducts });
+    // let products = all();
+    // let indexProducts = products.filter((product) => product.index == "1");
+    // res.render("index", { indexProducts });
+    db.Product.findAll({
+      where: {
+        index: 1,
+      },
+    })
+      .then((indexProducts) => res.render("index", { indexProducts }))
+      .catch(() => {
+        res.render("index");
+      });
   },
 
   index: (req, res) => {
     /* let products = all();
     return res.render("products/productList", { products }); */
     let products = db.Product.findAll();
-    const success = data => res.render("products/productList", {products:data})
-    const error = error => res.send(error);
-    return products.then(success).catch()
+    const success = (data) =>
+      res.render("products/productList", { products: data });
+    const error = (error) => res.send(error);
+    return products.then(success).catch();
   },
 
   show: (req, res) => {
@@ -31,9 +41,10 @@ const controller = {
     return res.render("products/productDetail", {product: null});*/
 
     let product = db.Product.findByPk(req.params.sku);
-    const success = data => res.render("products/productDetail", {product:data})
-    const error = error => res.send(error);
-    return product.then(success).catch()    
+    const success = (data) =>
+      res.render("products/productDetail", { product: data });
+    const error = (error) => res.send(error);
+    return product.then(success).catch();
   },
 
   create: (req, res) => {
@@ -45,7 +56,7 @@ const controller = {
       req.body.image = req.files[0].filename;
     } else {
       req.body.image = "/img/products/logo4.png";
-    } 
+    }
     /* let nuevo = generate(req.body);
     let todos = all();
     todos.push(nuevo);
@@ -55,15 +66,15 @@ const controller = {
     let save = db.Product.create({
       name: req.body.name,
       description: req.body.description,
-      color:req.body.color,
+      color: req.body.color,
       size_id: req.body.size,
       price: parseInt(req.body.price),
       image: req.body.image,
-      index: req.body.index ? req.body.index : "0"
+      index: req.body.index ? req.body.index : "0",
     });
 
-    const success = data => res.redirect("/productos");
-    const error = error => res.send(error);
+    const success = (data) => res.redirect("/productos");
+    const error = (error) => res.send(error);
 
     return save.then(success).then(error);
   },
