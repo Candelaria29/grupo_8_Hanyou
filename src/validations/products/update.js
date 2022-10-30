2c42bdc7b7ee77c37a54e5f860386e455ef0a216
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-10-30 01:58:40 -0300

    Agrego el archivo de validaciones update.js

---
+++ src/validations/products/update.js
@@ -0,0 +1,33 @@
+const { body } = require("express-validator");
+
+let name = body("name")
+  .notEmpty()
+  .withMessage("El campo no puede estar vacío")
+  .bail()
+  .isLength({ min: 5 })
+  .withMessage("El campo debe contener al menos cinco caracteres");
+
+let description = body("description")
+  .notEmpty()
+  .withMessage("El campo no puede estar vacío")
+  .bail()
+  .isLength({ min: 20 })
+  .withMessage("El campo debe contener al menos veinte caracteres");
+
+let price = body("price")
+  .notEmpty()
+  .withMessage("El campo no puede estar vacío")
+  .bail()
+  .isLength({ max: 6 })
+  .withMessage(
+    "El campo no debe contener mas de cuatro enteros y dos decimales"
+  )
+  .bail()
+  .isNumeric()
+  .withMessage("El campo debe contener un número");
+
+let image = 0;
+
+let validator = [name, description, price];
+
+module.exports = validator;
