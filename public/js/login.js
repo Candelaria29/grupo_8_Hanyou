0912541379c73287b66fb1b7cd104cc765f1774d
Candelaria29 <candelariabarrios@hotmail.com>
2022-11-01 15:37:13 -0300

    validaciones login

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+let form = document.querySelector('#loginBox')
+
+form.addEventListener('submit', event => {
+event.preventDefault();
+let errors = [];
+
+let email = document.querySelector('#userEmail');
+
+if (email.value == "") {
+    errors.push("El campo 'Correo electrónico' no puede estar vacío.");
+    email.classList.add('errorForm')
+} else if (!email.value.includes('@')) {
+    errors.push("Por favor, ingrese un Email válido.");
+    email.classList.add('errorForm')
+}
+
+let password = document.querySelector('#passwordLogin');
+
+if (password.value == ""){
+    errors.push("El campo 'Contraseña' no puede estar vacío.");
+    password.classList.add('errorForm')
+}
+
+console.log(errors);
+
+if (errors.length > 0) {
+    let errorsList = document.querySelector('div#errors ul');
+    errors.forEach( error => errorsList.innerHTML += "<li class='error'> " + error + "</li>")
+} else {
+    event.target.submit();
+
+
+})
