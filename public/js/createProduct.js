c72db16563cb0bbabd02cbc14c4e2c128cc34458
Candelaria29 <candelariabarrios@hotmail.com>
2022-11-01 21:05:18 -0300

    estilo en errores create y update

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -34,7 +34,7 @@ form.addEventListener('submit', function (event) {
 
     if (errors.length > 0) {
         let errorsList = document.querySelector('div#errors ul');
-        errors.forEach(error => errorsList.innerHTML += "<li> " + error + "</li>")
+        errors.forEach(error => errorsList.innerHTML += "<li> <i class='fa-solid fa-circle-exclamation'></i> " + error + "</li>")
     } else {
         event.target.submit();
     }
