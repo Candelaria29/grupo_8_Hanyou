dae0647b98e78bb945c2ef5b0e8c83755df988e5
Candelaria29 <candelariabarrios@hotmail.com>
2022-11-09 20:02:57 -0300

    instalo router dom

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import Sidebar from './components/sidebar';
 import Main from './components/main';
 
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
