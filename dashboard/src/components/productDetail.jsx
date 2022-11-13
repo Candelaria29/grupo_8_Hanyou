6e8618e953b8db64b099520a9c1dabc6e0b15911
NICNAC1995 <nicolasagustincilio@gmail.com>
2022-11-13 20:07:49 -0300

    Consumi la API desde el productDetail.jsx

--- dashboard/src/components/productDetail.jsx
+++ dashboard/src/components/productDetail.jsx
@@ -1,12 +1,26 @@
 import "../css/productDetail.css";
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function ProductDetail(props) {
   let { id } = useParams();
+  const [product, setProduct] = useState([]);
+  const API = `http://localhost:8000/api/products/${id}`;
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => {
+        setProduct(data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
   return (
     <div id="detailMain">
       <p>Detalle de producto</p>
       <p>{id}</p>
+      <p>{product.color}</p>
     </div>
   );
 }
